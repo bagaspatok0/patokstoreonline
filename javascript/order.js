c7ba90df8207599293148d8831b3e27ad72d6129
bagaspatok <88608788+bagaspatok0@users.noreply.github.com>
2024-06-29 10:41:48 +0700

    Update order.js

--- javascript/order.js
+++ javascript/order.js
@@ -1,15 +1,34 @@
-function checkout() {
+function getTimeBasedGreeting() {
+    const now = new Date();
+    const hours = now.getHours();
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Selamat pagi";
+    } else if (hours < 18) {
+        greeting = "Selamat siang";
+    } else {
+        greeting = "Selamat malam";
+    }
+
+    return greeting;
+}
+
+
+function checkout() {  
     console.log('checkout clicked'); // Debugging
 
     // Ambil data keranjang dari Local Storage
     let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    const timemessage = getTimeBasedGreeting();
 
     let message = 'Saya ingin memesan produk berikut:\n';
     cart.forEach(item => {
         message += `- ${item.name} (${item.price})\n`;
     });
     const encodedMessage = encodeURIComponent(message);
-    const whatsappUrl = `https://wa.me/+628990763604?text=${encodedMessage}`;
+    const whatsappUrl = `https://wa.me/+628990763604?text=Hallo ${timemessage}, ${encodedMessage}`;
     console.log(whatsappUrl); // Debugging
     window.open(whatsappUrl, '_blank');
 }
